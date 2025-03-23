1bf234c68efddadab3af8b57ac1cb8e38a4dac0b
Kristian <crystal.cook07@gmail.com>
2025-03-23 12:35:19 -0400

    fixed wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,3 +2,6 @@ from mistai import app  # Import your Flask app
 
 # Gunicorn/AlwaysData looks for a variable named "application"
 application = app  # Rename "app" to "application" for WSGI compatibility
+
+if __name__ == "__main__":
+    application.run()
