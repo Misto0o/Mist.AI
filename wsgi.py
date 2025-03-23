fe2b45406849e23fc21c283b5ce846d409011574
Kristian <crystal.cook07@gmail.com>
2025-03-23 13:11:02 -0400

    Revert "V4 is out"
    This reverts commit 92e7879edc055bf1692041d56e3032dd39038615.

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,3 @@ from mistai import app  # Import your Flask app
 
 # Gunicorn/AlwaysData looks for a variable named "application"
 application = app  # Expose the "app" as "application" for WSGI compatibility
-
-if __name__ == "__main__":
-    application.run(debug=False, host="0.0.0.0", port=5000, use_reloader=False)
